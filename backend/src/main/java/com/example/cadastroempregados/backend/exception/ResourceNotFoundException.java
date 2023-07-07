524ab683f4b98876559c417b0f1f4a73ab8a070d
jaddizim <jaddizim@gmail.com>
2023-07-07 14:52:26 -0300

    Creating ListEmployeeComponent

--- backend/src/main/java/com/example/cadastroempregados/backend/exception/ResourceNotFoundException.java
+++ backend/src/main/java/com/example/cadastroempregados/backend/exception/ResourceNotFoundException.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.io.Serial;
-import java.io.Serializable;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
