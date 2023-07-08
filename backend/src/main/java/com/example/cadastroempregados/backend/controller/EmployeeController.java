01edd44afc1d51e4c44b81ff57ec410b3aa269a0
Jadder <jaddizim@gmail.com>
2023-07-08 16:09:30 -0300

    Creating insert Employee Rest API

--- backend/src/main/java/com/example/cadastroempregados/backend/controller/EmployeeController.java
+++ backend/src/main/java/com/example/cadastroempregados/backend/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.example.cadastroempregados.backend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,15 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    //Get all employees
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+
+    //Insert an employee
+    @PostMapping("/employees")
+    public Employee insertEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
     }
 }
