efe0bc41517ea757eed04795c971dd7d58dfa875
jaddizim <jaddizim@gmail.com>
2023-07-14 16:29:50 -0300

    Creating Find Employee by Id Rest API

--- backend/src/main/java/com/example/cadastroempregados/backend/controller/EmployeeController.java
+++ backend/src/main/java/com/example/cadastroempregados/backend/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.example.cadastroempregados.backend.controller;
 
+import com.example.cadastroempregados.backend.exception.ResourceNotFoundException;
 import com.example.cadastroempregados.backend.model.Employee;
 import com.example.cadastroempregados.backend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,15 +23,26 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    //Get all employees
+    // Get all employees
     @GetMapping
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
-    //Insert an employee
+    // Get an employee by ID
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Funcionário não encontrado com o id " + id));
+
+        return ResponseEntity.ok(employee);
+    }
+
+    // Insert an employee
     @PostMapping
-    public Employee insertEmployee(@RequestBody Employee employee){
+    public Employee insertEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
     }
+
 }
